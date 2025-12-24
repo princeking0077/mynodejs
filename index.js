634c3efb79bfa2a981de5c390d9dbfd4638a071c
shoai <shoai@hostel.com>
2025-12-24 09:03:31 +0530

    Fix: Add root index.js bridge for Hostinger deployment structure

---
+++ index.js
@@ -0,0 +1,5 @@
+// Hostinger Entry Point (Bridge)
+// This file exists to satisfy Hostinger's "Entry File: index.js" requirement
+// since the actual application logic has been moved to the /server directory.
+
+require('./server/index.js');
